f2ba4fcb8a502e64c7d5929436858f6890772d52
kiuind <kiuind@gmail.com>
2021-09-06 00:10:17 -0300

    salvamento de arquivos

--- server.js
+++ server.js
@@ -15,9 +15,8 @@ for (var i = 0; i < string.length ; i++) {
 console.log(valorfinal)
 return valorfinal;
 }
-
-const http = require('http').Server(app),
-ms = require('mediaserver');
+var formidable = require('formidable');
+const http = require('http').Server(app)
 
 const serverSocket = require('socket.io')(http)
 
@@ -37,6 +36,30 @@ app.get('/', function (requisicao, resposta) {
     //ms.pipe(requisicao, resposta,[__dirname + '/index.html',__dirname +  "/som.mp3"]);
 resposta.sendFile(__dirname + '/index.html')
 })
+var fs = require('fs');
+
+app.use('/fileupload/', function (req, res) {
+    if (req.url == '/fileupload') {
+        var form = new formidable.IncomingForm();
+        form.parse(req, function (err, fields, files) {
+          var oldpath = files.filetoupload.path;
+          var newpath = __dirname + "/public/" + files.filetoupload.name;
+          fs.rename(oldpath, newpath, function (err) {
+            if (err) throw err;
+            res.write('File uploaded and moved!');
+            res.end();
+          });
+     });
+      } else {
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.write('<form action="fileupload" method="post" enctype="multipart/form-data">');
+        res.write('<input type="file" name="filetoupload"><br>');
+        res.write('<input type="submit">');
+        res.write('</form>');
+        return res.end();
+      }})
+
+
 
 app.use('/secret/',function (req,res) {
     console.log("chamando")
